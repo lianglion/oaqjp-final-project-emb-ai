e0bbc5a29747480e89f4a0de0b998da99e2b63f1
lianglion <liang.l.li@hotmail.com>
2025-01-19 00:40:46 -0500

    Chapter 4

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,19 @@
+import requests
+# import json
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json = myobj, headers=header)
+    emotion_data = response.json()
+    emotion_scores = emotion_data['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    result = {
+        'anger': emotion_scores['anger'],
+        'disgust': emotion_scores['disgust'],
+        'fear': emotion_scores['fear'],
+        'joy': emotion_scores['joy'],
+        'sadness': emotion_scores['sadness'],
+        'dominant_emotion': dominant_emotion
+    }
+    return result
