123730751772298deb6306b51f393d7b5ab79d68
lianglion <liang.l.li@hotmail.com>
2025-01-19 01:36:59 -0500

    verify chap4

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,5 +1,4 @@
 import requests
-# import json
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyze } }
